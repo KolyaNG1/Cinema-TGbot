5e5313dd93821f82a75170deee57780f6efe0e6a
C_R_U_X <97001576+KolyaNG1@users.noreply.github.com>
2026-04-29 13:09:15 +0300

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,7 +1,7 @@
 import os
 
 def get_api_token():
-    return os.getenv("BOT_TOKEN")#'8527253571:AAF13zajIyNruBA6J1_zy2oox_Q57-THbEk'
+    return os.getenv("BOT_TOKEN")
 
 def get_poiskkino_token():
-    return os.getenv('KINOPOISK_TOKEN')#'48TYJN0-JCGMHBR-JPK297V-KT1BXQM'
+    return os.getenv('KINOPOISK_TOKEN')
